34049e05234573a98e8668173ca6a7ab8cd692d0
ezreal <ezreal_rao@163.com>
2025-08-23 14:36:26 +0800

    🦄 refactor: 调整代码结构

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use zero2prod::run;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let _settings = get_configuration().expect("Failed to read configuration.");
     let listener = std::net::TcpListener::bind("127.0.0.1:8081")?;
     run(listener)?.await
 }
